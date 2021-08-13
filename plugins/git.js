433f35eae69efec331a9ce8d6bff46f396d09bf6
Nadeem(mx_walker) <88842663+Mxwalker342@users.noreply.github.com>
2021-08-13 23:32:04 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -19,7 +19,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Ameer Suhail*
-*owner number wa.me/919946432377*
+*owner number wa.me/919074208013*
 *bot setting video : https://youtu.be/_D4ZYuUSXjs
 *githublink       https://github.com/ameer-kallumthodi/pikachu*
 *audio commads    https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
